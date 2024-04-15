82db714cc2e984808cfb10481579676a9429e57a
tanishksahni <tanishk.sahni2002@gmail.com>
2024-04-15 15:27:50 +0530

    Initial Commit

---
+++ OpeninAppProject/OpeninAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeninAppProjectApp.swift
+//  OpeninAppProject
+//
+//  Created by Tanishk Sahni on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct OpeninAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
